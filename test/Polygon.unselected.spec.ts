96eee645ee80c4bd3973725ee2006528d697466c
Fredrik Kemling <kemling@gmail.com>
2020-04-25 23:14:56 +0200

    Added tests for UnselectedState-class

---
+++ test/Polygon.unselected.spec.ts
@@ -0,0 +1,69 @@
+import { expect } from 'chai';
+import { Polygon } from '../built/Polygon';
+
+
+describe('Polygon - unselected', () => {
+
+    it('handleLeftClick() - unselect', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 });
+        expect(square.isSelected).to.equal(false);
+    });
+
+    it('handleLeftClick() - reselect', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 }); // unselecting
+        square.handleLeftClick({ x: 100, y: 110 }); // reselecting
+        expect(square.isSelected).to.equal(true);
+    });
+
+    it('handleLeftClick() failing to reselect', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 }); // unselecting
+        square.handleLeftClick({ x: 10, y: 10 }); // reselecting
+        expect(square.isSelected).to.equal(false);
+    });
+
+
+    it('handleLeftClick() failing to reselect', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 }); // unselecting
+        square.handleRightClick({ x: 10, y: 10 }); // reselecting
+        // right clicking has no effect
+    });
+
+    it('calculateSegments', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 }); // unselecting
+        expect(square.segments.length).to.equal(4);
+    });
+
+    it('calculatePaintableStillSegments()', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 }); // unselecting
+        expect(square.getPaintableStillSegments().length).to.equal(4);
+    });
+
+    it('calculatePaintableMovingSegments()', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 }); // unselecting
+        expect(square.getPaintableMovingSegments({ x: 10, y: 10 }).length).to.equal(0);
+    });
+
+    it('calculatePaintableMovingSegments()', () => {
+        const square: Polygon = getSquare();
+        square.handleLeftClick({ x: 10, y: 10 }); // unselecting
+        expect(square.getPaintableMovingSegments({ x: 10, y: 10 }).length).to.equal(0);
+    });
+
+});
+
+function getSquare(): Polygon {
+    const square: Polygon = new Polygon();
+    square.handleLeftClick({ x: 100, y: 100 });
+    square.handleLeftClick({ x: 200, y: 100 });
+    square.handleLeftClick({ x: 200, y: 200 });
+    square.handleLeftClick({ x: 100, y: 200 });
+    square.handleLeftClick({ x: 100, y: 100 });
+    return square;
+}
