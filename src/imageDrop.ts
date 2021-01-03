c21039e5ca614f14a43ff7bc7887b28cae794056
Fredrik Kemling <kemling@gmail.com>
2021-01-03 19:38:02 +0100

    refactoring

--- src/imageDrop.ts
+++ src/imageDrop.ts
@@ -1,4 +1,7 @@
 let imageFile: File;
+const imageCanvas: HTMLCanvasElement = document.querySelector('canvas#imageLayer');
+const filterCanvas: HTMLCanvasElement = document.querySelector('canvas#filterLayer');
+const imageContext: CanvasRenderingContext2D = imageCanvas.getContext('2d');
 
 export function dropHandler(ev: DragEvent): void {
     ev.preventDefault();
@@ -8,8 +11,8 @@ export function dropHandler(ev: DragEvent): void {
         readImageFileAndDrawToCanvas(file);
     } else {
         alert(file.type + ' is not supported');
-        imageFile = null; // TODO: Ok att rensa tidigare bild?
-        clearTheCanvas(); // -- // --
+        imageFile = null;   // TODO: Ok att rensa tidigare bild?
+        clearImageCanvas(); // -- // --
     }
 }
 
@@ -24,6 +27,8 @@ function readImageFileAndDrawToCanvas(file: File): void {
     reader.readAsDataURL(file);
     reader.onload = (): void => {
         const image: HTMLImageElement = new Image();
+        console.log('image.width:    ' + image.width);
+        console.log('image.height:   ' + image.height);
         image.src = reader.result.toString();
         image.onload = (): void => {
             drawImageToCanvas(image);
@@ -31,33 +36,42 @@ function readImageFileAndDrawToCanvas(file: File): void {
     };
 }
 
-function clearTheCanvas(): void {
-    const canvas: HTMLCanvasElement = document.querySelector('canvas');
-    const context: CanvasRenderingContext2D = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
+function clearImageCanvas(): void {
+    imageContext.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
 }
 
 function drawImageToCanvas(image: HTMLImageElement): void {
-    const canvas: HTMLCanvasElement = document.querySelector('canvas');
-    console.log('canvas.width: ' + canvas.width);
-    console.log('canvas.clientWidth: ' + canvas.clientWidth);
-    console.log('canvas.height: ' + canvas.height);
-    console.log('canvas.clicentHeight: ' + canvas.clientHeight);
-    console.log('image.width:    ' + image.width);
-    console.log('image.height:   ' + image.height);
+    imageContext.drawImage(image, 0, 0, image.width, image.height,
+         0, 0, imageCanvas.clientWidth, imageCanvas.clientHeight);
+}
+
+export function dragOverHandler(ev: DragEvent): void { ev.preventDefault(); }
+
+export function adjustCanvas(): void {
+    console.log('Resizing canvases');
+
+    imageCanvas.width = window.innerWidth * 0.9;
+    imageCanvas.height = window.innerHeight * 0.8;
+
+    filterCanvas.width = window.innerWidth * 0.9;
+    filterCanvas.height = window.innerHeight * 0.8;
+
+
+    //console.log('canvas.width: ' + canvas.width);
+    //console.log('canvas.clientWidth: ' + canvas.clientWidth);
+    //console.log('canvas.height: ' + canvas.height);
+    //console.log('canvas.clicentHeight: ' + canvas.clientHeight);
+
 
     //canvas.width = image.width;
     //canvas.height = image.height;
-    const context: CanvasRenderingContext2D = canvas.getContext('2d');
-    //context.filter = 'blur(10px)';
-    context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.clientWidth, canvas.clientHeight);
+    
+    if (imageFile !== null) {
+        readImageFileAndDrawToCanvas(imageFile);
+    }
 }
 
-export function dragOverHandler(ev: DragEvent): void {
-    console.log('File(s) in drop zone');
-    ev.preventDefault();
-}
-
+export const delayedAdjustCanvas: any = debounce((): void => adjustCanvas(), 250);
 
 function debounce<F extends Function>(func: F, wait: number): F {
     let timeoutID: number;
@@ -68,16 +82,4 @@ function debounce<F extends Function>(func: F, wait: number): F {
             func.apply(context, args);
         }, wait);
     };
-}
-
-export const delayedResizeCanvas: any = debounce((): void => resizeCanvas(), 250);
-
-export function resizeCanvas(): void {
-    console.log('Resizing canvas');
-    const theCanvas: HTMLCanvasElement = document.querySelector('canvas');
-    theCanvas.width = window.innerWidth * 0.9;
-    theCanvas.height = window.innerHeight * 0.8;
-    if (imageFile !== null) {
-        readImageFileAndDrawToCanvas(imageFile);
-    }
 }
