67e11003716e3cdd043b8babe8282b193c59b39c
Fredrik Kemling <kemling@gmail.com>
2021-01-20 15:41:58 +0100

    minor bug fix

--- src/painter/RulerPainter.ts
+++ src/painter/RulerPainter.ts
@@ -59,7 +59,9 @@ export class RulerPainter extends AbstractSegmentPainter {
     }
 
     private jumpToNewPoint(point: Point, direction: Vector, distance: number): Point {
-        if (direction.norm > 1) { console.warn('direction given is not a unit vector ' + direction.norm); };
+        if (Math.abs(direction.norm-1) > 0.000001) { 
+            console.warn('direction given is not a unit vector. Has length: ' + direction.norm); 
+        };
         const newPointX: number = point.x + direction.x * distance;
         const newPointY: number = point.y + direction.y * distance;
         return new Point(newPointX, newPointY);
