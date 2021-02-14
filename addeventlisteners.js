17df20d183d8bb1256842d5bf87154d909d64c79
Fredrik Kemling <kemling@gmail.com>
2021-02-14 21:07:09 +0100

    refactoring

--- addeventlisteners.js
+++ addeventlisteners.js
@@ -7,30 +7,30 @@ import {
 } from './built/imageDrop.js';
 
 document.getElementById('foreground')
-    .addEventListener('click', (event) => { canvasLeftClicked(event, event.target.id); });
+    .addEventListener('click', (event) => canvasLeftClicked(event, event.target.id));
 document.getElementById('foreground')
-    .addEventListener('mousemove', (event) => { canvasMouseMovement(event, event.target.id); });
+    .addEventListener('mousemove', (event) => canvasMouseMovement(event, event.target.id));
 document.getElementById('foreground')
-    .addEventListener('mousedown', (event) => { canvasMouseDown(event, event.target.id); });
+    .addEventListener('mousedown', (event) => canvasMouseDown(event, event.target.id));
 document.getElementById('foreground')
-    .addEventListener('mouseup', (event) => { canvasMouseUp(event, event.target.id); });
+    .addEventListener('mouseup', (event) => canvasMouseUp(event, event.target.id));
 document.getElementById('foreground')
-    .addEventListener('contextmenu', (event) => { canvasRightClicked(event, event.target.id); });
+    .addEventListener('contextmenu', (event) => canvasRightClicked(event, event.target.id));
 document.getElementById('addPositivePolygon')
-    .addEventListener('click', () => { addShape('PositivePolygonArea'); });
+    .addEventListener('click', () => addShape('PositivePolygonArea'));
 document.getElementById('addNegativePolygon')
-    .addEventListener('click', () => { addShape('NegativePolygonArea'); });
+    .addEventListener('click', () => addShape('NegativePolygonArea'));
 document.getElementById('addRuler')
-    .addEventListener('click', () => { addShape('Ruler'); });
+    .addEventListener('click', () => addShape('Ruler'));
 document.getElementById('removeSelectedShape')
-    .addEventListener('click', () => { removeSelectedShape(); });
+    .addEventListener('click', () => removeSelectedShape());
 
 
-addEventListener('load', () => { adjustCanvas(); });
-addEventListener('resize', () => { delayedAdjustCanvas(); });
+addEventListener('load', () => adjustCanvas());
+addEventListener('resize', () => delayedAdjustCanvas());
 document.getElementById('viewport')
-    .addEventListener('drop', (event) => { dropHandler(event); });
+    .addEventListener('drop', (event) => dropHandler(event));
 document.getElementById('viewport')
-    .addEventListener('dragover', (event) => { dragOverHandler(event); });
+    .addEventListener('dragover', (event) => dragOverHandler(event));
 document.getElementById('opacitySlider')
-    .addEventListener('change', (event) => { adjustOpacity(event.target.value); });
+    .addEventListener('change', (event) => adjustOpacity(event.target.value));
