a99c716fce9db82378eb6dc5baa9683e3abaa8e0
Fredrik Kemling <kemling@gmail.com>
2021-09-16 19:40:04 +0200

    webpack also fixes css + cache busting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './style.css';
 import {
     addShape, removeSelectedShape, canvasMouseMovement, canvasLeftClicked,
     canvasRightClicked, canvasMouseDown, canvasMouseUp
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+    new HtmlWebpackPlugin({ template: './src/template.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+};
