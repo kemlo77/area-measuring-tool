57fb68768f8835ad648f4db7bd3e555614cc7670
Fredrik Kemling <kemling@gmail.com>
2023-02-09 13:49:24 +0100

    cypress in github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
       // implement node event listeners here
     },
     'baseUrl': 'http://localhost:5600',
